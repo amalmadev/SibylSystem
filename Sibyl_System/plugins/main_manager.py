73a217722daae1add8b16382414ad0e19e2e6863
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-04 17:00:11 +0530

    Update main_manager.py

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -87,9 +87,10 @@ async def proof(event):
             
 help_plus ="""
 Here is the help for **Main**:
-scan - Reply to a message with reason to send a request for gbans
-approve - Approve a scan request
-proof - Get message from proof id which is at the end of gban msg 
+
+`scan` - Reply to a message with reason to send a request for gbans
+`approve` - Approve a scan request
+`proof` - Get message from proof id which is at the end of gban msg 
 """
 
 __plugin_name__ = "Main" 
