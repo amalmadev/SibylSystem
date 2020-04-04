a6fb01d28249ff298013099e5bb1acb968da72dd
animekaizoku <animekaizokux@gmail.com>
2020-04-04 23:12:51 +0530

    updated strings and replies

--- Sibyl_System/strings.py
+++ Sibyl_System/strings.py
@@ -1,22 +1,25 @@
-on_string = "Sibyl-System Is on, We should die."
+on_string = "Sibyl-System is now active!"
 help_string = """
-Here is the for Sibyl-System (All the cmd can be used with ?,!,.)
-**scan** - send request for gban
-**status** - check if bot is online
-**help** - Peace. 
-**approve** - Approve gban request
-**proof** - get proof from id 
+Here is the for Sibyl-System (All the cmd can be used with ?,!,. and /)
+**scan** - Forces a Cymatic scan on the user.
+**status** - Check connection to Sibyl System.
+**approve** - Approves Sibyl Dominator request.
+**proof** - Pulls past scans data from an ID.
+**help** - Lists Information to use Sibyl.
 """
 scan_request_string = """
 $SCAN
-{enforcer} is scanning user {spammer}
+{enforcer} is requesting Cymatic Scan for {spammer}
 Reason: {reason}
 Message: {message} 
 """
 
 scan_approved_string = """
-$KILL
+#LethalEliminator
 **Enforcer** = {enforcer}
-**Scanned User** = {scam}
+**Target User** = {scam}
 **Approved By** = {approved_by}
+**Crome Coefficient**= Over 300
 """
+# https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
+# https://psychopass.fandom.com/wiki/The_Dominator
