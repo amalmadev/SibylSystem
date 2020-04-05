dac4143b7c4f516a01b761a74613df5255ab9305
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-05 23:53:29 +0530

    Update users.py

--- Sibyl_System/plugins/users.py
+++ Sibyl_System/plugins/users.py
@@ -1,1 +1,18 @@
+from Sibyl_System import SIBYL, ENFORCERS
+from Sibyl_System import System
+import asyncio
+from telethon import events
 
+@System.on(events.NewMessage(pattern=r'[\.\?!/]addenf'))
+async def addenf(event):
+  if event.from_id in SIBYL:
+     if event.reply:
+        replied = await event.get_reply_message()
+        id = reply.from_id
+     else:
+        id = event.text.split(" ", 2)[1]
+     if id in ENFORCERS:
+           await System.send_message(event.chat_id, 'That person is already Enforcer!')
+           return
+     ENFORCERS.append(id)
+     await System.send_message(event.chat_id, 'Done!) 
