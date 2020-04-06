b8f8ec01a589b251aa46b283624834008039aa91
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-06 10:11:43 +0530

    Added rmenf and list users

--- Sibyl_System/plugins/users.py
+++ Sibyl_System/plugins/users.py
@@ -15,4 +15,37 @@ async def addenf(event):
            await System.send_message(event.chat_id, 'That person is already Enforcer!')
            return
      ENFORCERS.append(id)
-     await System.send_message(event.chat_id, 'Done!') 
+     await System.send_message(event.chat_id, f'Added [{id}](tg://user?id={id}) to Enforcers') 
+
+@System.on(events.NewMessage(pattern=r'[\.\?!/]rmenf'))
+async def rmenf(event):
+  if event.from_id in SIBYL:
+     if event.reply:
+        replied = await event.get_reply_message()
+        id = replied.sender.id
+     else:
+        id = event.text.split(" ", 2)[1]
+     if id in ENFORCERS:
+           ENFORCERS.append(id)
+           await System.send_message(event.chat_id, f'Removed [{id}](tg://user?id={id}) from Enforcers') 
+           return
+     await System.send_message(event.chat_id, 'Is that person even a Enforcer?') 
+
+@System.on(events.NewMessage(pattern=r'[\.\?!/]listusers'))
+async def listuser(event):
+  if event.from_id in SIBYL:
+      msg = "Sibyl users:\n" 
+      for x in SIBYL:
+         try:
+           user = await System.get_entity(x)
+           msg += f"•[{user.first_name}](tg://user?id={user.id})\n"
+         except:
+           msg += f"•{user.id}"
+      msg += "Enforcers:\n"
+      for x in ENFORCERS:
+         try:
+           user = await System.get_entity(x)
+           msg += f"•[{user.first_name}](tg://user?id={user.id})\n"
+         except:
+           msg += f"•{user.id}"
+      await System.send_message(event.chat_id, msg)
