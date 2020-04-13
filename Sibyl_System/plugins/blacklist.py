4142c6614896653a04aa1867b6fd29290ae50b47
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-14 00:31:14 +0530

    Add rm blacklist and duplicate blacklist handling

--- Sibyl_System/plugins/blacklist.py
+++ Sibyl_System/plugins/blacklist.py
@@ -7,10 +7,18 @@ db = MONGO_CLIENT['Sibyl']['Main']
 upd = {} 
 owo = {}
 
-def add_to_blacklist(word):
+def add_to_blacklist(word, add = False):
      bl = db.find_one({'_id': 1})
      current = bl['blacklisted']
-     current.append(word)
+     If add:
+        if word in current: 
+             return False
+        current.append(word)
+     else:
+        if word in current:
+          current.remove(word)
+        else: 
+          return False
      upd['blacklisted'] = current
      owo['$set'] = upd
      db.update_one(bl, owo)
@@ -22,5 +30,22 @@ async def addenf(event):
        text = event.text.split(" ", 1)[1]
      except:
        return 
-     add_to_blacklist(text)
-     await System.send_message(event.chat_id, f"Added {text} to blacklist") 
+     a = add_to_blacklist(text, add = True)
+     if a:
+        await System.send_message(event.chat_id, f"Added {text} to blacklist") 
+     else:
+        await System.send_message(event.chat_id, f" {text} is already blacklisted") 
+
+@System.on(events.NewMessage(pattern=r'[\.\?!]rmbl'))
+async def addenf(event):
+  if event.from_id in SIBYL:
+     try:
+       text = event.text.split(" ", 1)[1]
+     except:
+       return 
+     a = add_to_blacklist(text, add = False)
+     if a:
+        await System.send_message(event.chat_id, f"Removed {text} from blacklist") 
+     else:
+        await System.send_message(event.chat_id, f" {text} is not blacklisted") 
+
