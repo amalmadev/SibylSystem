a75c578f59aee1a1916acbf682d51d936da908cd
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-13 20:16:38 +0530

    with ah yeah

--- Sibyl_System/plugins/blacklist.py
+++ Sibyl_System/plugins/blacklist.py
@@ -4,11 +4,11 @@ from Sibyl_System import MONGO_CLIENT, System, SIBYL, ENFORCERS
 db = MONGO_CLIENT['Sibyl']['Main']
 #cant find better names
 upd = {} 
-with = {}
+owo = {}
 def add_to_blacklist(word):
      bl = db.find_one({'_id': 1})
      current = bl['blacklisted']
      current.append(word)
      upd['blacklisted'] = current
-     with['$set'] = upd
+     owo['$set'] = upd
      db.update_one(with, bl)
