dcd398254f86ef2348d0f5b0e314889e4ec01408
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-13 10:46:41 +0530

    coroutine object is not subscriptable

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -104,7 +104,7 @@ async def proof(event):
                   await msg.edit(f" Failed to get proof, Is the proof id valid?")
                   return
      async with session.get('https://nekobin.com/api/documents', json = {'content': message}) as r:
-           paste = f"https://nekobin.com/{r.json()['result']['key']}"
+           paste = f"https://nekobin.com/{(await r.json())['result']['key']}"
      await msg.edit(f"**Proof from ID**[`{proof_id}`]:\n**Reason**: {reason}\n**Message**: `{paste}`") 
 
 reject_string ="""
