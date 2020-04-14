f3d1fbaae2f27236f62d142a3bca4b04fc235bbd
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-14 22:13:40 +0530

    Forgot to remove escape part

--- Sibyl_System/plugins/blacklist.py
+++ Sibyl_System/plugins/blacklist.py
@@ -69,7 +69,7 @@ async def auto_gban_request(event):
     sender = await event.get_sender()
     if words:
       for word in words:
-          pattern = r"( |^|[^\w])" + re.escape(word) + r"( |$|[^\w])"
+          pattern = r"( |^|[^\w])" + word + r"( |$|[^\w])"
           if re.search(pattern, text, flags=re.IGNORECASE):
                   await System.send_message(Sibyl_logs, f"$AUTO\nTriggered by: [{event.from_id}](tg://user?id={event.from_id})\nMessage: {event.text}") 
 __plugin_name__ ="blacklist" 
