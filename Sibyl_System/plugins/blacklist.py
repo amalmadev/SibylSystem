699f95753c4a8d0f7fce1652af83fb236fd65721
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-14 15:15:05 +0530

    UNEXPECTED EOF

--- Sibyl_System/plugins/blacklist.py
+++ Sibyl_System/plugins/blacklist.py
@@ -66,4 +66,4 @@ async def auto_gban_request(event):
       for word in words:
           pattern = r"( |^|[^\w])" + re.escape(word) + r"( |$|[^\w])"
           if re.search(pattern, text, flags=re.IGNORECASE):
-                  await System.send_message(Sibyl_logs, f"$AUTO\nTriggered by: [{event.from_id}](tg://user?id={event.from_id})\nMessage: {event.text}"
+                  await System.send_message(Sibyl_logs, f"$AUTO\nTriggered by: [{event.from_id}](tg://user?id={event.from_id})\nMessage: {event.text}") 
