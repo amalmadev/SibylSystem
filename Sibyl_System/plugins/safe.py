aafcce8153b91c0d93c14f564832649839e69d44
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-17 19:55:29 +0530

    Added shutdown and reboot
    If breaks don't tag

--- Sibyl_System/plugins/safe.py
+++ Sibyl_System/plugins/safe.py
@@ -1,1 +1,22 @@
 
+from telethon import events
+import asyncio
+import os
+import sys
+import re
+
+@System.on(events.NewMessage(pattern = r"[!/\?\.]sibyl restart"))
+async def reboot(event):
+    if event.fwd_from:
+        return
+    await System.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+
+
+@System.on(events.NewMessage(pattern = r"[!/\?\.]sibyl shutdown"))
+async def shutdown(event):
+    if event.fwd_from:
+        return
+    await System.send_message(event.chat_id, "Shutting Down... ")
+    await System.disconnect()
